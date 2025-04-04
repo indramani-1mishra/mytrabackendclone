d266f62e9a44141a6635ee263cb3fabefcee4393
RAJ <rajmishra.ydd@gmail.com>
2025-04-04 13:46:04 +0530

    product uploded to cloudnery

--- src/routes/version1/products.js
+++ src/routes/version1/products.js
@@ -15,10 +15,12 @@ const { isLoggedIn, isAdmin } = require('../../validetor/islogin');
 const productrouter = express.Router();
 
 // Create a product
-productrouter.post('/', isLoggedIn, isAdmin, upload.single('image'), productcontrollerc);
 
 // Get all products
 productrouter.get("/", getAllProductsController);
+
+productrouter.post('/', isLoggedIn, isAdmin, upload.single('image'), productcontrollerc);
+
 
 // Get products by category - should be before :id
 productrouter.get("/category/:category", getProductsByCategoryc);
